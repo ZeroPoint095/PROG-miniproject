43697b7aeb3753ceb012c9e1fbe1eb981d8cfa49
Roel <roelmei22@gmail.com>
2016-11-08 21:55:39 +0100

    opmaak aangepast + while loop

--- Vertragingen.py
+++ Vertragingen.py
@@ -6,31 +6,29 @@ import xmltodict
 from API_NS import (auth_details,
                     api_url)
 
-response = requests.get(api_url, auth=auth_details)
-
-dienstRegelingXML = xmltodict.parse(response.text)
-
 def vertragingen():
-
+    print('Informatie vertragingen: ')
     for treinRitten in dienstRegelingXML['ActueleVertrekTijden']['VertrekkendeTrein']:
 
         #print('VertrekVertraging' in treinRitten)
         #print(treinRitten)
 
         if 'VertrekVertraging' in treinRitten:
-
-                print('De volgende trajecten hebben vertraging:')
                 VertrekTijd = treinRitten['VertrekTijd']
                 VertrekTijd = VertrekTijd[11:16]
-                VertrekVertragingTekst = treinRitten['VertrekVertragingTekst'] # 2016-09-27T18:36:00+0200
+                VertrekVertragingTekst = treinRitten['VertrekVertragingTekst']
                 EindBestemming = treinRitten['EindBestemming']
                 TreinSoort = treinRitten['TreinSoort']
                 RouteTekst = treinRitten['RouteTekst']
+                while RouteTekst != RouteTekst:
+                    continue
                 print('De ' + TreinSoort + ' naar ' + RouteTekst + ' met als eindbestemming ' + EindBestemming + ' van ' + VertrekTijd + ' heeft een vertraging van ' + VertrekVertragingTekst + '.')
         else:
                 'Er zijn op dit moment geen vertragingen bekend.'
 
     return vertragingen
 
+response = requests.get(api_url, auth=auth_details)
+dienstRegelingXML = xmltodict.parse(response.text)
 vertragingen()
 
