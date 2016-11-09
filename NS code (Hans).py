157eb7abcce7cc976d5a56327827f2ea753580e4
hansrijksen <hans1997@mail.com>
2016-11-09 12:47:52 +0100

    Update

--- NS code (Hans).py
+++ NS code (Hans).py
@@ -1,15 +1,16 @@
 import requests
 import xmltodict
 from API_NS import (auth_details, api_url_storingen)
+import html
 
 response = requests.get(api_url_storingen, auth=auth_details)
 storingXML = xmltodict.parse(response.text)
-
 
 def gepland():
     for storing in storingXML['Storingen']['Gepland']['Storing']:
         traject = storing['Traject']
         periode = storing['Periode']
-        print('Traject: ', traject, '\n' 'Periode: ', periode)
+        bericht = storing['Bericht']
+        print(bericht)
 
 gepland()
