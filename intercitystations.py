25eb7166648b17c06ff3457b265673a8b7dc297d
qhoogwerf <qhoogwerf@gmail.com>
2016-11-09 20:28:18 +0100

    reisplannert

--- intercitystations.py
+++ intercitystations.py
@@ -1,29 +1,42 @@
 import requests
 import xmltodict
 from API_NS import (auth_details)
+from collections import defaultdict
 
 api_url = 'http://webservices.ns.nl/ns-api-stations-v2'
 response = requests.get(api_url, auth=auth_details)
 
 ic = xmltodict.parse(response.text)
-icList=[]
 
-for station in ic['Stations']['Station']:
-    if station['Land'] == 'NL':
-        if station['Type'] == 'knooppuntIntercitystation' or station['Type'] == 'intercitystation':
-            icList.append(station['Namen']['Lang'])
+def makeIntercityStationList():
+    icList=[]
+    for station in ic['Stations']['Station']:
+        if station['Land'] == 'NL':
+            if station['Type'] == 'knooppuntIntercitystation' or station['Type'] == 'intercitystation':
+                if station['Namen']['Lang'][0]=='\'':
+                    icList.append(station['Synoniemen']['Synoniem'][1])
+                else:
+                    icList.append(station['Namen']['Lang'])
+    return icList
 
-print(icList)
+def makeIntercityStationDict():
+    stationsAlfa = defaultdict(list)
+    for station in icList:
+        stationsAlfa[station[0]].append(station)
+    return stationsAlfa
 
-from itertools import groupby
-from operator import itemgetter
-from collections import defaultdict
+icList=makeIntercityStationList()
+icAlfaDict=makeIntercityStationDict()
 
-stationsAlfa=defaultdict(list)
-for letter, words in groupby(sorted(icList), key=itemgetter(0)):
-    for key, value in icList:
-        stationsAlfa[key].append(value)
-    for word in words:
-        lijst_stations_tijdelijk+=[word]
-        print (word)
-print(stationsAlfa)
+#mislukte code
+# from itertools import groupby
+# from operator import itemgetter
+# from collections import defaultdict
+
+
+# stationsAlfa=defaultdict(list)
+# for letter, station in groupby(sorted(icList), key=itemgetter(0)):
+#     for letter, word in icList:
+#         stationsAlfa[letter].append(word)
+
+
