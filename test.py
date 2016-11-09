de736dc9c0f938a695e1c1220fa453d681d20d39
qhoogwerf <qhoogwerf@gmail.com>
2016-11-09 20:53:18 +0100

    reisplannert

--- test.py
+++
@@ -1,9 +0,0 @@
-print('Nein')
-
-api_url = 'http://webservices.ns.nl/ns-api-storingen?station=ut'
-response = requests.get(api_url, auth=auth_details)
-
-storingenXML = xmltodict.parse(response.text)
-
-print('Dit zijn de treinen met een storing: ')
-for storing in storingenXML[]
