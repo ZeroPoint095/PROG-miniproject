cee0a93f23bf01c41da93c0c64b683e51ff758c5
007codemaster <ardjan.b@gmail.com>
2016-11-09 13:42:05 +0100

    final version

--- NS Code (Ard-Jan) v2.py
+++ NS Code (Ard-Jan) v2.py
@@ -3,11 +3,10 @@ import xmltodict
 from API_NS import (auth_details, api_url)
 
 response = requests.get(api_url, auth=auth_details)
-storingXML = xmltodict.parse(response.text)
-
 vertrekkenXML = xmltodict.parse(response.text)
+
 def actuele_vertrekinformatie():
-    print('Dit zijn de vertrekkende treinen het komende uur')
+    uitvoer=['Dit zijn de vertrekkende treinen het komende uur']
     for vertrek in vertrekkenXML['ActueleVertrekTijden']['VertrekkendeTrein']:
         vertraging = []
         route = []
@@ -27,6 +26,11 @@ def actuele_vertrekinformatie():
         else:
             route = 'geen tussenstations'
 
-        print('Om ' + vertrektijd + ' vertrekt een trein (' + treintype+  ') naar ' + eindbestemming + ' van spoor ' + spoor + ' met een eventuele vertraging van: ' + vertraging + '.'
-              + '\n '+  ' De eventuele tussenstations zijn: ' + route)
+        uitvoer_avt = 'Om '+vertrektijd + ' vertrekt een trein (' + treintype+  ') naar ' + eindbestemming + ' van spoor ' + spoor + ' met een eventuele vertraging van: ' + vertraging + '.'
+        uitvoer_avt2 = ' De eventuele tussenstations zijn: ' + route
+        #print(uitvoer_avt)
+        uitvoer += [uitvoer_avt]
+        uitvoer += [uitvoer_avt2]
+    #print(uitvoer)
+    return uitvoer
 actuele_vertrekinformatie()
