12840cf8e2dae2f386bca16eb225d780b46ad5cc
007codemaster <ardjan.b@gmail.com>
2016-11-09 12:55:02 +0100

    v0.2

--- NS Code (Ard-Jan) v2.py
+++ NS Code (Ard-Jan) v2.py
@@ -1,34 +1,32 @@
 import requests
 import xmltodict
+from API_NS import (auth_details, api_url)
 
-email = 'email'
-wachtwoord = 'ww'
-
-auth_details = (email, wachtwoord)
-api_url = 'https://webservices.ns.nl/ns-api-avt?station=ut'
 response = requests.get(api_url, auth=auth_details)
+storingXML = xmltodict.parse(response.text)
 
 vertrekkenXML = xmltodict.parse(response.text)
+def actuele_vertrekinformatie():
+    print('Dit zijn de vertrekkende treinen het komende uur')
+    for vertrek in vertrekkenXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+        vertraging = []
+        route = []
+        eindbestemming = vertrek['EindBestemming']
 
-print('Dit zijn de vertrekkende treinen')
-for vertrek in vertrekkenXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-    vertraging = []
-    route = []
-    eindbestemming = vertrek['EindBestemming']
+        vertrektijd = vertrek['VertrekTijd']
+        vertrektijd = vertrektijd[11:16]
+        treintype = vertrek['TreinSoort']
+        spoor = vertrek['VertrekSpoor']['#text']
+        if 'VertrekVertraging' in vertrek:
+                    VertrekVertragingTekst = vertrek['VertrekVertragingTekst']
+                    vertraging = VertrekVertragingTekst
+        else:
+            vertraging = 'geen vertraging'
+        if 'RouteTekst' in vertrek:
+                    route = vertrek['RouteTekst']
+        else:
+            route = 'geen tussenstations'
 
-    vertrektijd = vertrek['VertrekTijd']
-    vertrektijd = vertrektijd[11:16]
-    treintype = vertrek['TreinSoort']
-    spoor = vertrek['VertrekSpoor']['#text']
-    if 'VertrekVertraging' in vertrek:
-                VertrekVertragingTekst = vertrek['VertrekVertragingTekst']
-                vertraging = VertrekVertragingTekst
-    else:
-        vertraging = 'geen vertraging'
-    if 'RouteTekst' in vertrek:
-                route = vertrek['RouteTekst']
-    else:
-        route = 'geen tussenstations'
-
-    print('Om ' + vertrektijd + ' vertrekt een trein (' + treintype+  ') naar ' + eindbestemming + ' van spoor ' + spoor + ' met een eventuele vertraging van: ' + vertraging + '.'
-          + ' De eventuele tussenstations zijn: ' + route)
+        print('Om ' + vertrektijd + ' vertrekt een trein (' + treintype+  ') naar ' + eindbestemming + ' van spoor ' + spoor + ' met een eventuele vertraging van: ' + vertraging + '.'
+              + '\n '+  ' De eventuele tussenstations zijn: ' + route)
+actuele_vertrekinformatie()
