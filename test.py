5bf73f02e4d16763091712f2b36eb859b656a657
hansrijksen <hans1997@mail.com>
2016-11-09 12:11:34 +0100

    Update

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print('Nein')
+
+api_url = 'http://webservices.ns.nl/ns-api-storingen?station=ut'
+response = requests.get(api_url, auth=auth_details)
+
+storingenXML = xmltodict.parse(response.text)
+
+print('Dit zijn de treinen met een storing: ')
+for storing in storingenXML[]
