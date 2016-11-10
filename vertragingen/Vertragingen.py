6afd04c2b56b74928b73606f81d47181f3e4670f
Roel <roelmei22@gmail.com>
2016-11-10 20:54:38 +0100

    Break verwijderd uit code en else omgezet naar if statement met indents.

--- vertragingen/Vertragingen.py
+++ vertragingen/Vertragingen.py
@@ -10,8 +10,8 @@ def vertragingen():
     Uitvoer_vertragingen = []
     #For-loop die XML data controleerd op vertragingen.
     for treinRitten in dienstRegelingXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-        print('VertrekVertragingTekst' in treinRitten)
-        print(treinRitten)
+        #print('VertrekVertragingTekst' in treinRitten)
+        #print(treinRitten)
         #Als er vertragingen zijn wordt deze IF-statement uitgevoerd.
         if 'VertrekVertragingTekst' in treinRitten:
 
@@ -29,10 +29,10 @@ def vertragingen():
             TreinSoort = treinRitten['TreinSoort']
 
             Uitvoer_vertragingen += ['De ' + TreinSoort + ' naar ' + TussenStations + EindBestemming + ' van ' + VertrekTijd + ' heeft een vertraging van ' + Vertraging + '.']
-        #Als er geen vertragingen in XML data aanwezig zijn wordt dit weergegeven.
-        elif 'VertrekVertragingTekst' not in treinRitten:
-            Uitvoer_vertragingen += ['Er zijn op dit moment geen vertragingen bekend.']
-        break
+            #Als er geen vertragingen in XML data aanwezig zijn wordt dit weergegeven.
+            if 'VertrekVertragingTekst' not in treinRitten:
+                Uitvoer_vertragingen += ['Er zijn op dit moment geen vertragingen bekend.']
+
     print(Uitvoer_vertragingen)
     #Uitvoer van code versturen naar hoofdprogramma.
     return Uitvoer_vertragingen
