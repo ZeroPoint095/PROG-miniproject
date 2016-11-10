5698e201cc0c1ccc09c2e33ace3afc9855152ed2
qhoogwerf <qhoogwerf@gmail.com>
2016-11-10 14:22:36 +0100

    reisplannert

--- reisplanner/reisplanner.py
+++ reisplanner/reisplanner.py
@@ -2,26 +2,40 @@ import requests
 import xmltodict
 from API_NS import (auth_details)
 from datetime import datetime
-
+import time
 
 huidigeStation='Utrecht Centraal'
-bestemming='Woerden'
+bestemming='Rotterdam Noord'
 
-api_url = 'http://webservices.ns.nl/ns-api-treinplanner?fromStation='+huidigeStation+'&toStation='+bestemming+'&departure=trueexterne'
-response = requests.get(api_url, auth=auth_details)
+#online xml
+# api_url = 'http://webservices.ns.nl/ns-api-treinplanner?fromStation='+huidigeStation+'&toStation='+bestemming+'&departure=trueexterne'
+# response = requests.get(api_url, auth=auth_details)
+# reisplannerXML = xmltodict.parse(response.text)
 
-reisplannerXML = xmltodict.parse(response.text)
+## offline xml
+with open('offlinetest.xml') as xmlfile:
+    reisplannerXML=xmltodict.parse(xmlfile.read(),dict_constructor=dict)
+
+
 
 def optimaalReis():
     'Returnt de eerste de beste vertrektijd, spoor nummer en treinsoort'
     datetime.now
     for xml in reisplannerXML['ReisMogelijkheden']['ReisMogelijkheid']:
         if xml['Optimaal'] == 'true':
-            tijd=xml['ActueleVertrekTijd']
-            tijd=tijd[11:16]
-            spoor=xml['ReisDeel']['ReisStop'][0]['Spoor']['#text']
-            trein=xml['ReisDeel']['VervoerType']
-    return tijd, spoor, trein
+            overstap = 0
+            overstapStation =[]
+            while xml['AantalOverstappen'] != overstap:
+                overstappenIn=xml['ReisDeel'][0]['Reisstop'][0]['Naam']
+                overstapStation.append(overstappenIn)
+                overstap+=1
+            tijd=xml['ActueleVertrekTijd'] #actuele vertrektijd volgens de NS API
+            tijd=datetime.strptime(tijd, "%Y-%m-%dT%H:%M:%S%z")#converteert ISO tijd naar datetime tijd
+            tijd=datetime.strftime(tijd, '%H:%M') #split de tijd in enkel uren en minuten
+            #tijdOver=datetime.strftime(tijd, '%H:%M')-datetime.now()
+            spoor=xml['ReisDeel'][0]['ReisStop'][0]['Spoor']['#text']
+            trein=xml['ReisDeel'][0]['VervoerType']
+    return tijd, spoor, trein, #overstapStation
 
 # ## mislukte xml parse pogingen ###
 # optimaal = reisplannerXML['ReisMogelijkheden']['ReisMogelijkheid']['Optimaal']
