55c9de187cf05d47d8077379117b4594a6087910
Roemei <roelmei22@gmail.com>
2016-11-11 11:37:25 +0100

    v3.3 color verwijderd, opdracht gefinetuned

--- vertragingen/Vertragingen.py
+++ vertragingen/Vertragingen.py
@@ -6,19 +6,20 @@ import xmltodict
 from API_NS import (auth_details,
                      api_url)
 
+#Voor debuggen.
 # with open('offlinevert.xml') as myXMLfile:
 #      dienstRegelingXML = xmltodict.parse(myXMLfile.read(),dict_constructor=dict)
 
 def vertragingen():
     Uitvoer_vertragingen = []
-    #For-loop die XML data controleerd op vertragingen.
+    #For-loop die XML-data controleerd op vertragingen.
     for treinRit in dienstRegelingXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-        #print('VertrekVertragingTekst' in treinRit)
-        #print(treinRit)
-        #Als er vertragingen zijn wordt deze IF-statement uitgevoerd.
+        #print('VertrekVertragingTekst' in treinRit) debug print
+        #print(treinRit) debug print
+        #Vertragingen worden door deze IF-statement aan de uitvoerlijst toegevoegd.
         if 'VertrekVertragingTekst' in treinRit:
 
-            #If en else statements voor het geval trajecten geen tussenstations hebben.
+            #If en else statements voor het geval trajecten geen XML-element tussenstations hebben.
             if 'RouteTekst' in treinRit:
                 TussenStations = ' met tussenstation(s) ' + treinRit['RouteTekst']
 
@@ -30,18 +31,18 @@ def vertragingen():
             Vertraging = treinRit['VertrekVertragingTekst']
             EindBestemming = treinRit['EindBestemming']
             TreinSoort = treinRit['TreinSoort']
-            #Voegt alle vertragingen toe aan uitvoerlijst.
+            #Voegt alle gevonden vertragingen toe aan uitvoerlijst.
             Uitvoer_vertragingen += ['De ' + TreinSoort + ' naar ' + EindBestemming + TussenStations + ' van ' + VertrekTijd + ' heeft een vertraging van ' + Vertraging + '.']
 
     #Als er geen vertragingen aan de uitvoerlijst zijn toegevoegd wordt dit aan de lijst toegevoegd.
     if len(Uitvoer_vertragingen) == 0:
         Uitvoer_vertragingen = ['Er zijn op dit moment geen vertragingen bekend.']
 
-    print(Uitvoer_vertragingen)
+    #print(Uitvoer_vertragingen) debug print
     #Uitvoerlijst versturen naar hoofdprogramma.
     return Uitvoer_vertragingen
 
 response = requests.get(api_url, auth=auth_details)
 dienstRegelingXML = xmltodict.parse(response.text)
-vertragingen()
+
 
