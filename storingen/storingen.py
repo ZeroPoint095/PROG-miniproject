f23dfc169ab1ed96446b18f8341a36111b268cc9
hansrijksen <hans1997@mail.com>
2016-11-11 10:37:11 +0100

    Update

--- storingen/storingen.py
+++ storingen/storingen.py
@@ -2,24 +2,24 @@ import requests
 import xmltodict
 from API_NS import (auth_details, api_url_storingen)
 import re
-
+#Hier wordt het XML-bestand aangeroepen uit het bestand API_NS.py
 response = requests.get(api_url_storingen, auth=auth_details)
 storingXML = xmltodict.parse(response.text)
 
+#Hier begint de Functie die alle storingen ophaalt
 def storingen_ophalen():
 
+    #Uitvoer is een lege list
     uitvoerSTR = []
     try:
-        for storing in storingXML['Storingen']['Gepland']['Storing']:
-            traject = storing['Traject']
-            bericht = storing['Bericht']
-            bericht_new = re.sub("<.*?>", "", bericht)
-            storingen_uitvoer = 'Traject:' + traject + bericht_new
-            uitvoerSTR += [storingen_uitvoer]
-        for item in uitvoerSTR:
-            print(item)
+        for storing in storingXML['Storingen']['Gepland']['Storing']:       #Loopt door de xml file
+            traject = storing['Traject']                                    #Haalt uit de key storing de tag Traject
+            bericht = storing['Bericht']                                    #Haalt uit de key storing de tag Bericht
+            bericht_new = re.sub("<.*?>", "", bericht)                      #Maakt van de variabele Bericht, een nieuwe variabele zonder de Html-tags
+            storingen_uitvoer = 'Traject:' + traject + bericht_new          #Voegt Traject en het bericht samen tot een bericht
+            uitvoerSTR += [storingen_uitvoer]                               #Hierin worden de berichten samen met het traject toegevoegt aan een list
     except:
-        uitvoerSTR = ['Er zijn geen storingen']
+        uitvoerSTR = ['Er zijn geen storingen']                             #Als er geen keys traject en bericht voorkomen, wordt er een foutmelding laten zien
 
     return uitvoerSTR
 
