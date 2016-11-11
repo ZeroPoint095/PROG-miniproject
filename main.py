861925af27b175aca7aca8cfcf8c787a68be130e
MarkHeeren <mark.heeren@student.hu.nl>
2016-11-11 13:32:52 +0100

    update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 from Vertragingen import vertragingen
 from actueleVertrektijden.actuele_vertrektijden import actuele_vertrekinformatie
+from reisplanner import reisplanner
+from reisplanner.intercitystations import returnStations
 from storingen import storingen_ophalen_gepland
 from storingen import storingen_ophalen_ongepland
 
@@ -24,32 +26,34 @@ class App:
         frame3 = Frame(master)
         frame3.pack(side=RIGHT)
 
+        frame4 = Frame(frame3)
+        frame4.pack(side=RIGHT)
+
         scrollbar = Scrollbar(frame3)
         scrollbar.pack( side=RIGHT, fill=Y )
 
         self.button = Button(frame, text="QUIT", fg="red", command=frame.quit, bg="yellow")
         self.button.pack(side=TOP, fill=X)
 
-        self.quentin_hoofdmenu_knop = Button(frame, text="Quentin", command=self.quentin, bg="yellow", fg="blue")
-        self.quentin_hoofdmenu_knop.pack(side=TOP, fill=X)
+        self.reisplanner_hoofdmenu_knop = Button(frame, text="Reisplanner", command=self.reisplanner, bg="yellow", fg="blue")
+        self.reisplanner_hoofdmenu_knop.pack(side=TOP, fill=X)
 
-        self.actuele_vertrektijden_hoofdmenu_knop = Button(frame, text="Ard-Jan", command=self.actuele_vertrektijden, bg="yellow", fg="blue")
+        self.actuele_vertrektijden_hoofdmenu_knop = Button(frame, text="Actuele Vertrektijden", command=self.actuele_vertrektijden, bg="yellow", fg="blue")
         self.actuele_vertrektijden_hoofdmenu_knop.pack(side=TOP, fill=X)
 
-        self.vertragingen_hoofdmenu_knop = Button(frame, text="vertraging", command=self.vertraging, bg="yellow", fg="blue")
+        self.vertragingen_hoofdmenu_knop = Button(frame, text="Vertraging", command=self.vertraging, bg="yellow", fg="blue")
         self.vertragingen_hoofdmenu_knop.pack(side=TOP, fill=X)
 
         self.storingen_hoofdmenu_knop = Button(frame, text="Storingen", command=self.Storingen, bg="yellow", fg="blue")
         self.storingen_hoofdmenu_knop.pack(side=TOP, fill=X)
 
-    def quentin(self):
+    def reisplanner(self):
 
-        global frame
         global frame2
-        global quentin
+        global reisplanner
 
-        if quentin:
-            self.quentin_weghalen()
+        if reisplanner:
+            self.reisplanner_weghalen()
         if actuele_vertrektijden:
             self.actuele_vertrektijden_weghalen()
         if vertraging:
@@ -57,32 +61,61 @@ class App:
         if Storingen:
             self.storingen_weghalen()
 
-        self.quentin_weghalen_knop = Button(frame2, text="hoofdmenu", fg="red", command=self.quentin_weghalen, bg="yellow")
-        self.quentin_weghalen_knop.pack(side=TOP, fill=X)
+        self.reisplanner_weghalen_knop = Button(frame2, text="Hoofdmenu", fg="red", command=self.reisplanner_weghalen, bg="yellow")
+        self.reisplanner_weghalen_knop.pack(side=TOP, fill=X)
 
-        self.quentin_functie_knop = Button(frame2, text="Quentin functie", command=self.quentin_functie, bg="yellow", fg="blue")
-        self.quentin_functie_knop.pack(side=TOP, fill=X)
+        self.reisplanner_functie_knop = Button(frame2, text="Reisplanner", command=self.reisplanner_functie, bg="yellow", fg="blue")
+        self.reisplanner_functie_knop.pack(side=TOP, fill=X)
 
-        quentin=True
+        reisplanner=True
 
-    def quentin_weghalen(self):
-        self.quentin_weghalen_knop.destroy()
-        self.quentin_functie_knop.destroy()
+    def reisplanner_weghalen(self):
+        self.reisplanner_weghalen_knop.destroy()
+        self.reisplanner_functie_knop.destroy()
 
-        global quentin
-        quentin=False
+        global reisplanner
+        reisplanner=False
 
-    def quentin_functie(self):
-        print('Quentin')
+    def stationsweergeven(self, stationsletter):
+        global reisplanner_stationsweergegeven
+        global frame4
+
+        ICDICT=returnStations()
+
+        if reisplanner_stationsweergegeven:
+            for station in ICDICT[stationsletter]:
+                self.station_knop.destroy()
+
+        reisplanner_stationsweergegeven=True
+
+        for station in ICDICT[stationsletter]:
+            self.station_knop = Button(frame4, text=station, fg="red", bg="yellow", command=reisplanner('Utrecht Centraal', station))
+            self.station_knop.pack(side=TOP, padx=250)
+
+    def reisplanner_functie(self):
+
+        global reisplanner_weergegeven
+        global frame3
+
+        letters=["A", "B", "C", "D", "E", "G", "H", "L", "M", "N", "O", "R", "S", "T", "V", "W", "Z"]
+
+        if reisplanner_weergegeven:
+            for letter in letters:
+                self.letter_knop.destroy()
+
+        for letter in letters:
+            self.letter_knop = Button(frame3, text=letter, fg="red", bg="yellow", command=self.stationsweergeven(letter))
+            self.letter_knop.pack(side=TOP, padx=250)
+
+        reisplanner_weergegeven=True
 
     def actuele_vertrektijden(self):
 
-        global frame
         global frame2
         global actuele_vertrektijden
 
-        if quentin:
-            self.quentin_weghalen()
+        if reisplanner:
+            self.reisplanner_weghalen()
         if actuele_vertrektijden:
             self.actuele_vertrektijden_weghalen()
         if vertraging:
@@ -90,10 +123,10 @@ class App:
         if Storingen:
             self.storingen_weghalen()
 
-        self.actuele_vertrektijden_weghalen_knop = Button(frame2, text="hoofdmenu", fg="red", command=self.actuele_vertrektijden_weghalen, bg="yellow")
+        self.actuele_vertrektijden_weghalen_knop = Button(frame2, text="Hoofdmenu", fg="red", command=self.actuele_vertrektijden_weghalen, bg="yellow")
         self.actuele_vertrektijden_weghalen_knop.pack(side=TOP, fill=X)
 
-        self.actuele_vertrektijden_knop = Button(frame2, text="actuele_vertrektijden functie", command=self.actuele_vertrektijden_functie, bg="yellow", fg="blue")
+        self.actuele_vertrektijden_knop = Button(frame2, text="Actuele vertrektijden", command=self.actuele_vertrektijden_functie, bg="yellow", fg="blue")
         self.actuele_vertrektijden_knop.pack(side=TOP, fill=X)
 
         actuele_vertrektijden=True
@@ -109,7 +142,6 @@ class App:
         global actuele_vertrektijden
         actuele_vertrektijden=False
 
-
     def actuele_vertrektijden_functie(self):
         global scrollbar
         global actuele_vertrektijden_weergegeven
@@ -117,7 +149,7 @@ class App:
             self.actuele_vertrektijden_label.destroy()
             actuele_vertrektijden_weergegeven=False
         tekst=''
-        self.actuele_vertrektijden_label=Text(frame3, width=900, height=1000)
+        self.actuele_vertrektijden_label=Text(frame3, width=500, height=600)
         for vertrektijd in actuele_vertrekinformatie():
             self.actuele_vertrektijden_label.insert(END, vertrektijd)
         self.actuele_vertrektijden_label.pack(fill=BOTH)
@@ -130,8 +162,8 @@ class App:
         global frame2
         global vertraging
 
-        if quentin:
-            self.quentin_weghalen()
+        if reisplanner:
+            self.reisplanner_weghalen()
         if actuele_vertrektijden:
             self.actuele_vertrektijden_weghalen()
         if vertraging:
@@ -139,10 +171,10 @@ class App:
         if Storingen:
             self.storingen_weghalen()
 
-        self.vertraging_weghalen_knop = Button(frame2, text="hoofdmenu", fg="red", command=self.vertraging_weghalen, bg="yellow")
+        self.vertraging_weghalen_knop = Button(frame2, text="Hoofdmenu", fg="red", command=self.vertraging_weghalen, bg="yellow")
         self.vertraging_weghalen_knop.pack(side=TOP, fill=X)
 
-        self.vertraging_functie_knop = Button(frame2, text="vertraging functie", command=self.vertraging_functie, bg="yellow", fg="blue")
+        self.vertraging_functie_knop = Button(frame2, text="Vertragingen", command=self.vertraging_functie, bg="yellow", fg="blue")
         self.vertraging_functie_knop.pack(side=TOP, fill=X)
 
         vertraging=True
@@ -154,32 +186,37 @@ class App:
         self.vertraging_weghalen_knop.destroy()
         self.vertraging_functie_knop.destroy()
         if vertraging_weergegeven:
-            self.vertraging_label.destroy()
+            self.vertraging_weergeven.destroy()
             vertraging_weergegeven=False
 
         global vertraging
         vertraging=False
 
     def vertraging_functie(self):
-        global vertraging_weergegeven
-        if vertraging_weergegeven:
-            self.vertraging_label.destroy()
-            vertraging_weergegeven=False
-        tekst='Informatie vertragingen: '
-        for vertraging in vertragingen():
-            tekst+='\n'+vertraging
 
-        self.vertraging_label=Label(frame3, text=tekst)
-        self.vertraging_label.pack()
+        global frame3
+        global vertraging_weergegeven
+
+        if vertraging_weergegeven:
+            self.vertraging_weergeven.destroy()
+            vertraging_weergegeven=False
+
+        self.vertraging_weergeven = Text(frame3, width=500, height=600)
+        for vertraging in vertragingen():
+            self.vertraging_weergeven.insert(END, vertraging)
+        self.vertraging_weergeven.pack()
+
         vertraging_weergegeven=True
+
+        scrollbar.config(command=self.storingen_weergeven.yview)
 
     def Storingen(self):
 
         global frame2
         global Storingen
 
-        if quentin:
-            self.quentin_weghalen()
+        if reisplanner:
+            self.reisplanner_weghalen()
         if actuele_vertrektijden:
             self.actuele_vertrektijden_weghalen()
         if vertraging:
@@ -207,41 +244,26 @@ class App:
         Storingen=False
 
     def storingen_functie(self):
-        global frame3
-        global scrollbar
         global storingen_weergegeven
 
         if storingen_weergegeven:
             self.storingen_weergeven.destroy()
             storingen_weergegeven=False
 
-        storingen_weergegeven=True
-
-        #self.storing_weergeven = Canvas(frame3, yscrollcommand=scrollbar.set)#, bg='blue')
-        #self.storing_weergeven.config(width=1000, height=500)
-        #self.storing_weergeven.config(scrollregion=(500,0,1500, (250*len(storingen_ophalen()))))
-        #self.storing_weergeven.pack(expand=YES)
-
-        #i=0
-        #j=0
-        #for storing in storingen_ophalen_gepland():
-        #    self.storing_weergeven.create_text(900, 50+i*250, text=storing, width=700)
-        #    i+=1
-        #for storing in storingen_ophalen_ongepland():
-        #    self.storing_weergeven.create_text(900, 50+i*250+j*250, text=storing, width=700)
-        #    j+=1
-
-        self.storingen_weergeven = Text(frame3, width=900, height=1000)
+        self.storingen_weergeven = Text(frame3, width=500, height=600)
         for storing in storingen_ophalen_gepland():
             self.storingen_weergeven.insert(END, storing)
         for storing in storingen_ophalen_ongepland():
             self.storingen_weergeven.insert(END, storing)
-        self.storingen_weergeven.pack(fill=BOTH)
+        self.storingen_weergeven.pack()
 
         storingen_weergegeven=True
+
         scrollbar.config(command=self.storingen_weergeven.yview)
 
-quentin=False
+reisplanner=False
+reisplanner_weergegeven=False
+reisplanner_stationsweergegeven=False
 actuele_vertrektijden=False
 actuele_vertrektijden_weergegeven=False
 vertraging=False
